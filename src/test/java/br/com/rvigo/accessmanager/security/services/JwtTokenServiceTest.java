d6a0ab3e96a4fdc7d5b86428c2e7c19a27ecf19f
Rafael Vigo <rvigo07@gmail.com>
2021-12-18 20:32:04 -0300

    add more tests to JwtTokenService class

--- src/test/java/br/com/rvigo/accessmanager/security/services/JwtTokenServiceTest.java
+++ src/test/java/br/com/rvigo/accessmanager/security/services/JwtTokenServiceTest.java
@@ -2,14 +2,24 @@ package br.com.rvigo.accessmanager.security.services;
 
 import br.com.rvigo.accessmanager.entities.User;
 import br.com.rvigo.accessmanager.security.entities.Jwt;
+import br.com.rvigo.accessmanager.security.exceptions.JwtTokenException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
+import static java.lang.System.currentTimeMillis;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
@@ -19,6 +29,8 @@ public class JwtTokenServiceTest {
     private JwtTokenService service;
 
     private User user;
+    private String key;
+    private long tokenExpiration;
 
     @BeforeEach
     public void setUp() {
@@ -26,8 +38,9 @@ public class JwtTokenServiceTest {
         user.setId(UUID.randomUUID());
         user.setPassword("testPassword");
         user.setUsername("testUsername");
-        String key = "AVERYVERYLONGSTRINGTOMATCHNEEDEDBITSFORTHEHS512ALGORITHMACCORDINGTOJWTJWASPECIFIATION(RFC7518SECTION3.2)";
-        service = new JwtTokenService(3000L, key);
+        key = "AVERYVERYLONGSTRINGTOMATCHNEEDEDBITSFORTHEHS512ALGORITHMACCORDINGTOJWTJWASPECIFIATION(RFC7518SECTION3.2)";
+        tokenExpiration = 3000L;
+        service = new JwtTokenService(tokenExpiration, key);
     }
 
     @Test
@@ -44,7 +57,7 @@ public class JwtTokenServiceTest {
         Jwt jwt = service.generateToken(user);
 
         assertEquals("Bearer", jwt.getTokenType());
-        assertEquals(3000L, jwt.getDuration());
+        assertEquals(tokenExpiration, jwt.getDuration());
         assertNotNull(jwt.getJwtToken());
     }
 
@@ -63,9 +76,47 @@ public class JwtTokenServiceTest {
         newUser.setId(UUID.randomUUID());
 
         Jwt jwt = service.generateToken(newUser);
-
         Boolean notValidatedToken = service.validateToken(jwt.getJwtToken(), user);
 
         assertFalse(notValidatedToken);
+    }
+
+    @Test
+    public void shouldCreateATokenWhenUserHasGrantedAuthorities() {
+        user.setAuthorities(List.of(new SimpleGrantedAuthority("TEST_AUTHORITY")));
+        Jwt jwt = service.generateToken(user);
+
+        assertEquals("Bearer", jwt.getTokenType());
+        assertEquals(tokenExpiration, jwt.getDuration());
+        assertNotNull(jwt.getJwtToken());
+
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(key.getBytes(StandardCharsets.UTF_8)))
+                .build()
+                .parseClaimsJws(jwt.getJwtToken())
+                .getBody();
+
+        assertTrue(claims.containsKey("roles"));
+        assertEquals("TEST_AUTHORITY", claims.get("roles"));
+    }
+
+    @Test
+    public void shouldThrowJwtTokenExceptionWhenParsingToken() {
+        assertThrows(JwtTokenException.class, () -> service.validateToken("invalid_token", user)); //MalformedJwtException
+        assertThrows(JwtTokenException.class, () -> service.validateToken("", user)); //IllegalArgumentException
+
+
+        String expiredToken = Jwts.builder()
+                .setIssuedAt(new Date(currentTimeMillis()))
+                .setExpiration(new Date(currentTimeMillis()))
+                .signWith(Keys.hmacShaKeyFor(key.getBytes(StandardCharsets.UTF_8)), SignatureAlgorithm.HS512)
+                .compact();
+
+        assertThrows(JwtTokenException.class, () -> service.validateToken(expiredToken, user)); //ExpiredJwtException
+
+        String unsupportedToken = Jwts.builder()
+                .compact();
+
+        assertThrows(JwtTokenException.class, () -> service.validateToken(unsupportedToken, user)); //UnsupportedJwtException
     }
 }
